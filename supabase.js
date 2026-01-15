e6e9f717edc774f1fb291e51e42623a5b04a1366
liminaal <liminaal0425@gmail.com>
2026-01-15 18:53:26 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
+// supabase.js
+// Cliente global para usar en cualquier HTML
 
+const SUPABASE_URL = "https://niqcvctwikiluztlhkqs.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_98Kubj2Lfs9OV6Oj-EsrHQ_O2vszXUt";
+
+// Crear cliente y exponerlo globalmente
+window.supabase = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
